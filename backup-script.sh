983e22b35b5d5e5f0ba26c7d58d0f952c6f8cfde
kev <kev@darkhorse>
2023-09-12 13:20:05 +0100

    .

--- backup-script.sh
+++ backup-script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Script Summary:
-# This script iterates through a directory, creates tar archives of subdirectories,
-# and writes them to an LTO tape drive (/dev/st0). It keeps track of the number
-# of archives, the total size of the backups, and the duration of the backup process.
+# This script iterates through a directory, creates or updates tar archives
+# of subdirectories if needed, and writes them to an LTO tape drive (/dev/st0).
+# It keeps track of the number of archives created, the total size of
+# the backups, and the duration of the backup process.
 
 # DISCLAIMER: This script is provided as-is and without warranty.
 # Use it at your own risk. The script may result in data loss if
@@ -18,8 +19,8 @@ search_dir="/path/to/your/directory"
 # Define the target device
 target_device="/dev/st0"
 
-# Initialize a counter for the number of archives backed up
-archives_backed_up=0
+# Initialize a counter for archives created
+archives_created=0
 
 # Initialize a variable to track the total size in bytes
 total_size_bytes=0
@@ -34,6 +35,20 @@ check_tape_empty() {
     return 1  # Tape is not empty
   fi
 }
+
+# Function to count subdirectories in the search directory
+count_directories() {
+  local dir_count=0
+  for dir in "$1"/*; do
+    if [ -d "$dir" ]; then
+      ((dir_count++))
+    fi
+  done
+  echo "$dir_count"
+}
+
+# Calculate the total number of directories to tar
+total_dirs=$(count_directories "$search_dir")
 
 # Check if the target device exists
 if [ ! -e "$target_device" ]; then
@@ -58,19 +73,28 @@ fi
 # Record the start time
 start_time=$(date +%s)
 
+# Initialize a counter for directory progress
+dir_count=0
+
 # Iterate through subdirectories in the specified directory
 for dir in "$search_dir"/*; do
   if [ -d "$dir" ]; then
+    ((dir_count++))
     # Extract the directory name from the path
     dir_name=$(basename "$dir")
 
-    # Create a tar archive of the subdirectory and write it to the target device
-    tar cvf "$target_device" -C "$search_dir" "$dir_name"
+    # Check if the archive already exists and if it's older than the source directory
+    if [ -e "$target_device/$dir_name.tar" ] && [ "$dir" -nt "$target_device/$dir_name.tar" ]; then
+      # Update the existing archive
+      tar uvf "$target_device" -C "$search_dir" "$dir_name"
+    else
+      # Create a new archive
+      tar cvf "$target_device" -C "$search_dir" "$dir_name"
+      ((archives_created++))
+    fi
 
     # Check if the tar operation was successful
     if [ $? -eq 0 ]; then
-      ((archives_backed_up++))  # Increment the counter
-
       # Calculate the size of the archive in bytes and add it to the total
       archive_size_bytes=$(du -sb "$dir" | cut -f1)
       ((total_size_bytes += archive_size_bytes))
@@ -78,6 +102,9 @@ for dir in "$search_dir"/*; do
       echo "Error: Failed to backup $dir_name"
       exit 1
     fi
+
+    # Print progress
+    echo "Progress: $dir_count of $total_dirs directories processed"
   fi
 done
 
@@ -90,7 +117,7 @@ duration_minutes=$(( (end_time - start_time) / 60 ))
 # Convert the total size to gigabytes
 total_size_gb=$(bc <<< "scale=2; $total_size_bytes / (1024 * 1024 * 1024)")
 
-# Print the total archives backed up, total size in gigabytes, and the duration
-echo "Total archives backed up: $archives_backed_up"
+# Print the summary of archives created, total size, and the duration
+echo "Archives Created: $archives_created"
 echo "Total size archived: ${total_size_gb}GB"
 echo "Backup process completed in $duration_minutes minutes"
